40ae5178da2aa6c0128a6c31ed18e5f02aae656f
Torsten Werner <torsten.werner@westernacher.com>
2018-04-25 07:33:18 +0200

    add our own entity and repository

--- src/test/java/com/example/playmock/JooqTest.java
+++ src/test/java/com/example/playmock/JooqTest.java
@@ -3,12 +3,15 @@ package com.example.playmock;
 import com.example.playmock.jooq.tables.daos.AuthorDao;
 import com.example.playmock.jooq.tables.pojos.Author;
 import com.example.playmock.jooq.tables.records.AuthorRecord;
+import lombok.NonNull;
+import lombok.Value;
 import org.jooq.DSLContext;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.stereotype.Repository;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -25,6 +28,9 @@ public class JooqTest {
 
     private AuthorDao authorDao;
 
+    @Autowired
+    private AuthorRepository authorRepository;
+
     @Before
     public void setUp() throws Exception {
         authorDao = new AuthorDao(sql.configuration());
@@ -36,6 +42,13 @@ public class JooqTest {
                 sql.insertInto(AUTHOR, AUTHOR.AUTHOR_ID, AUTHOR.NAME)
                         .values(26, "Hildegunst von Mythenmetz")
                         .execute())
+                .isEqualTo(1);
+    }
+
+    @Test
+    public void shouldInsertFieldsWithRepo() {
+        assertThat(
+                authorRepository.insert(26, "Hildegunst von Mythenmetz"))
                 .isEqualTo(1);
     }
 
@@ -57,6 +70,14 @@ public class JooqTest {
     }
 
     @Test
+    public void shouldInsertEntityWithRepo() {
+        final AuthorEntity authorEntity = new AuthorEntity(26, "Hildegunst von Mythenmetz");
+        assertThat(
+                authorRepository.insert(authorEntity))
+                .isEqualTo(1);
+    }
+
+    @Test
     public void shouldInsertPojoWithDao() {
         assertThat(authorDao.count()).isEqualTo(0);
         final Author author = new Author(26, "Hildegunst von Mythenmetz");
@@ -65,5 +86,36 @@ public class JooqTest {
         assertThat(
                 authorDao.fetchOneByAuthorId(author.getAuthorId()))
                 .isEqualTo(author);
+    }
+}
+
+@Value
+class AuthorEntity {
+    @NonNull
+    Integer authorId;
+
+    @NonNull
+    String name;
+}
+
+interface AuthorRepository {
+    int insert(Integer authorID, String name);
+
+    default int insert(AuthorEntity authorEntity) {
+        return insert(authorEntity.getAuthorId(), authorEntity.getName());
+    }
+}
+
+@Repository
+class DefaultAuthorRepository implements AuthorRepository {
+
+    @Autowired
+    private DSLContext sql;
+
+    @Override
+    public int insert(Integer authorID, String name) {
+        return sql.insertInto(AUTHOR, AUTHOR.AUTHOR_ID, AUTHOR.NAME)
+                .values(authorID, name)
+                .execute();
     }
 }
