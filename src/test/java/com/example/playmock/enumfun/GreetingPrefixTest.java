15fbaf5ed8b5ecbe6d45960d5d91c90933751b18
Torsten Werner <torsten.werner@westernacher.com>
2018-04-30 19:22:13 +0100

    extend test

--- src/test/java/com/example/playmock/enumfun/GreetingPrefixTest.java
+++ src/test/java/com/example/playmock/enumfun/GreetingPrefixTest.java
@@ -9,7 +9,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class GreetingPrefixTest {
     @Test
     public void shouldGreetCorrectly() {
-        final Supplier<String> greeter = GreetingPrefix.HI.newGreeter("Hildegunst");
-        assertThat(greeter.get()).isEqualTo("Hi Hildegunst!");
+        final Supplier<String> greeter01 = GreetingPrefix.HI.newGreeter("Hildegunst");
+        assertThat(greeter01.get()).isEqualTo("Hi Hildegunst!");
+
+        final Supplier<String> greeter02 = GreetingPrefix.HELLO.newGreeter("Danzelot");
+        assertThat(greeter02.get()).isEqualTo("Hello Danzelot!");
     }
 }
