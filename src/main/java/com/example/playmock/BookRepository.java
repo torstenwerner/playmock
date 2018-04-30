dc63da9a77e108983e90c8217018177b4d6dd4c9
Torsten Werner <torsten.werner@westernacher.com>
2018-04-30 18:12:24 +0100

    add @Param annotation for spring jpa to avoid special javac arguments

--- src/main/java/com/example/playmock/BookRepository.java
+++ src/main/java/com/example/playmock/BookRepository.java
@@ -3,23 +3,24 @@ package com.example.playmock;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
+import org.springframework.data.repository.query.Param;
 
 public interface BookRepository extends Repository<Book, Integer> {
 
     @Query(value = "select * from book where book_id = :bookId", nativeQuery = true)
-    Book find(int bookId);
+    Book find(@Param("bookId") int bookId);
 
     @Modifying
     @Query(value = "insert into book(book_id, title, author_id) values (:#{#book.id}, :#{#book.title}, :#{#book.author.id})", nativeQuery = true)
-    void insert(Book book);
+    void insert(@Param("book") Book book);
 
     @Modifying
     @Query(value = "insert into author(author_id, name) VALUES (:#{#author.id}, :#{#author.name})", nativeQuery = true)
-    void insert(Author author);
+    void insert(@Param("author") Author author);
 
     @Query(value = "select b.title, a.name as authorName from book b join author a on b.author_id = a.author_id where b.book_id = :bookId", nativeQuery = true)
-    BookProjection findProjection(int bookId);
+    BookProjection findProjection(@Param("bookId") int bookId);
 
     @Query("select new com.example.playmock.BookDto(b.title, a.name) from Book b join Author a on b.author = a where b.id = :bookId")
-    BookDto findDto(int bookId);
+    BookDto findDto(@Param("bookId") int bookId);
 }
